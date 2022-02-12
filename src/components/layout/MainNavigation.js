23b14a2d445015cb11d009dde9fd4c89f8a62ffe
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-02-12 13:11:55 -0500

    test

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -6,7 +6,7 @@ import classes from './MainNavigation.module.css';
 function MainNavigation() {
     return (
         <header className={classes.header}>
-            <div className={classes.logo}>Workout App</div>
+            <div className={classes.logo}>Workout Apptest</div>
             <nav>
                 <ul>
                     <li><Link to='/'>New Workout</Link></li>
